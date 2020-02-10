2667c34f997675800a4039463c86cc097f1bb9f6
黄锦 <jin.huang02@hand-china.com>
2020-02-10 22:36:23 +0800

    feat(react-admin): add: 添加echarts图标相关

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -16,6 +16,7 @@ import Header from '../../components/header/header'
 import Product from '../product/product'
 import LeftNav from '../../components/left-nav/left-nav'
 import Category from '../category/category'
+import NotFound from '../not-found/not-found'
 
 
 const {Footer, Sider, Content} = Layout;
@@ -37,6 +38,8 @@ class Admin extends Component {
           <Header>Header</Header>
           <Content style={{backgroundColor: 'white', margin: '20px'}}>
             <Switch>
+              {/* 若访问 '/' ，则跳转到 '/home' 路径 */}
+              <Route exact from='/' to='home'/>
               <Route path='/home' component={Home}/>
               <Route path='/category' component={Category}/>
               <Route path='/product' component={Product}/>
@@ -45,7 +48,7 @@ class Admin extends Component {
               <Route path='/charts/bar' component={Bar}/>
               <Route path='/charts/line' component={Line}/>
               <Route path='/charts/pie' component={Pie}/>
-              <Redirect to='/home' />
+              <Route component={NotFound} /> {/* 当上面都不匹配的时候，则返回这个页面 */}
             </Switch>
           </Content>
           <Footer style={{textAlign: 'center', color: '#aaaaaa'}}>推荐使用谷歌浏览器，
